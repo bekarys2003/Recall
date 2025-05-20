395e99ec4f8ea295d8558728560500019a72449e
Bekarys <bek.arys@inbox.ru>
2025-05-19 22:07:07 -0700

    deploying

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,9 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://bekarys2003.github.io",
+    "https://bekarys2003.github.io"
 ]
 
 ALLOWED_HOSTS = [
@@ -133,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
